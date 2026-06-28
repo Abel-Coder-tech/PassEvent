e2987d18d0477664548bdda1a0c2a13b483236b8
Abel-Coder-tech <osseabel@gmail.com>
2026-06-28 09:51:37 +0100

    Favicon

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     <link rel="apple-touch-icon" href="{{ asset('images/logo_paxevent.png') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inscription — PaxEvent</title>
