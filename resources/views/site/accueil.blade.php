fd4c71545ccf401c105db97f45fdd774adee3a90
Abel-Coder-tech <osseabel@gmail.com>
2026-06-30 10:31:13 +0100

    Modification de la phrase d'accroche

--- resources/views/site/accueil.blade.php
+++ resources/views/site/accueil.blade.php
@@ -19,7 +19,7 @@
     <div class="container position-relative" style="z-index:2;">
         <div class="row g-5 align-items-center">
             <div class="col-lg-6 d-flex flex-column align-items-center justify-content-center text-center text-lg-start">
-                <span class="hero-chip">Billetterie 100% Bénin</span>
+                <span class="hero-chip">Billetterie en ligne 100% Bénin</span>
                 <h1 class="hero-title align-items-center justify-content-center text-center text-center">Achetez et vendez vos tickets en quelques clics</h1>
                 <p class="hero-subtitle align-items-center justify-content-center text-center">La solution simple et rapide pour gérer vos événements et vendre vos billets en ligne.</p>
                 <div class="hero-actions">
