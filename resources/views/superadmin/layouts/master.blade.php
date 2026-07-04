048e468e66b60688ab1e80a2ae82ef46eea04481
Abel-Coder-tech <osseabel@gmail.com>
2026-07-04 09:31:07 +0100

    Ajout du favicon pour mobile format png

--- resources/views/superadmin/layouts/master.blade.php
+++ resources/views/superadmin/layouts/master.blade.php
@@ -2,7 +2,9 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon.png') }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('favicon.png') }}">
     <link rel="apple-touch-icon" href="{{ asset('images/logo_paxevent.png') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Super Admin - PaxEvent')</title>
