e84254748c53c9b99966e8192a34702601600654
Abel <osseabel@gmail.com>
2026-09-23 19:34:55 +0100

    emails : planification queue:work toutes les minutes (sans chevauchement) pour vider la file

--- routes/console.php
+++ routes/console.php
@@ -9,6 +9,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
+Schedule::command('queue:work --tries=3 --timeout=60')->everyMinute()->withoutOverlapping();
 Schedule::command('evenements:terminer')->hourly();
 Schedule::command('tickets:purger-en-attente')->everyFifteenMinutes();
 Schedule::command('tickets:reconcilier')->hourly();
